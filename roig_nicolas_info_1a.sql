ee31926c9886e80046bfe8725f7aba44dd60a284
nicolasroig <nicolas.roigr@gmail.com>
2021-03-04 23:08:33 +0100

    EvaluationIntermediaire040321

--- roig_nicolas_info_1a.sql
+++ roig_nicolas_info_1a.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Lun 01 Mars 2021 à 21:22
+-- Généré le :  Jeu 04 Mars 2021 à 22:04
 -- Version du serveur :  5.7.11
 -- Version de PHP :  5.6.18
 
@@ -19,10 +19,9 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `roig_nicolas_info_1a`
 --
--- Database: NOM_PRENOM_INFO1X_SUJET_BD_104
--- Détection si une autre base de donnée du même nom existe
 
-DROP DATABASE if exists roig_nicolas_info_1a;
+-- --------------------------------------------------------
+DROP DATABASE IF EXISTS roig_nicolas_info_1a;
 
 -- Création d'un nouvelle base de donnée
 
@@ -31,8 +30,6 @@ CREATE DATABASE IF NOT EXISTS roig_nicolas_info_1a;
 -- Utilisation de cette base de donnée
 
 USE roig_nicolas_info_1a;
--- --------------------------------------------------------
-
 --
 -- Structure de la table `t_client`
 --
@@ -48,7 +45,13 @@ CREATE TABLE `t_client` (
 --
 
 INSERT INTO `t_client` (`Id_client`, `nom`, `prenom`) VALUES
-(1, 'Wick', 'John');
+(1, 'Wick', 'John'),
+(2, 'christian', 'stophe'),
+(3, 'christian', 'John'),
+(4, 'benteke', 'ronaldo'),
+(5, 'eden', 'hazard'),
+(6, 'philipp', 'coutinho'),
+(7, 'luis', 'suarez');
 
 -- --------------------------------------------------------
 
@@ -59,8 +62,19 @@ INSERT INTO `t_client` (`Id_client`, `nom`, `prenom`) VALUES
 CREATE TABLE `t_materiel` (
   `Id_materiel` int(11) NOT NULL,
   `materiel` tinytext NOT NULL,
-  `type_de_materiel` tinytext NOT NULL
+  `numero_de_serie` tinytext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `t_materiel`
+--
+
+INSERT INTO `t_materiel` (`Id_materiel`, `materiel`, `numero_de_serie`) VALUES
+(1, 'ecran', '45645356r'),
+(2, 'souris', '34645t'),
+(3, 'clavier', '435654r'),
+(4, 'clavier', '122123'),
+(5, 'pc portable', '3245024780620');
 
 -- --------------------------------------------------------
 
@@ -75,6 +89,17 @@ CREATE TABLE `t_preter_materiel` (
   `fk_materiel` int(11) NOT NULL,
   `fk_client` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `t_preter_materiel`
+--
+
+INSERT INTO `t_preter_materiel` (`Id_preter_materiel`, `Etat_avant_pret`, `date_pret`, `fk_materiel`, `fk_client`) VALUES
+(1, 'nickel', '2021-03-04 21:38:41', 1, 1),
+(2, 'tres propre', '2021-03-04 21:38:52', 2, 2),
+(3, 'une legere tache', '2021-03-04 21:39:13', 3, 3),
+(4, 'propre comme un sous neuf', '2021-03-04 21:39:33', 4, 4),
+(5, 'le matos pue un peu', '2021-03-04 21:39:49', 5, 5);
 
 -- --------------------------------------------------------
 
@@ -155,17 +180,17 @@ ALTER TABLE `t_technicien`
 -- AUTO_INCREMENT pour la table `t_client`
 --
 ALTER TABLE `t_client`
-  MODIFY `Id_client` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `Id_client` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT pour la table `t_materiel`
 --
 ALTER TABLE `t_materiel`
-  MODIFY `Id_materiel` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id_materiel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT pour la table `t_preter_materiel`
 --
 ALTER TABLE `t_preter_materiel`
-  MODIFY `Id_preter_materiel` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id_preter_materiel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT pour la table `t_retour_materiel`
 --
@@ -176,6 +201,24 @@ ALTER TABLE `t_retour_materiel`
 --
 ALTER TABLE `t_technicien`
   MODIFY `Id_technicien` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `t_preter_materiel`
+--
+ALTER TABLE `t_preter_materiel`
+  ADD CONSTRAINT `t_preter_materiel_ibfk_1` FOREIGN KEY (`fk_materiel`) REFERENCES `t_materiel` (`Id_materiel`),
+  ADD CONSTRAINT `t_preter_materiel_ibfk_2` FOREIGN KEY (`fk_client`) REFERENCES `t_client` (`Id_client`);
+
+--
+-- Contraintes pour la table `t_retour_materiel`
+--
+ALTER TABLE `t_retour_materiel`
+  ADD CONSTRAINT `t_retour_materiel_ibfk_1` FOREIGN KEY (`fk_materiel`) REFERENCES `t_materiel` (`Id_materiel`),
+  ADD CONSTRAINT `t_retour_materiel_ibfk_2` FOREIGN KEY (`fk_client`) REFERENCES `t_client` (`Id_client`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
